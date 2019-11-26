6adbcc655f727a5098865a5465f09972599bdc46
Jamie Cansdale <jcansdale@github.com>
2019-11-26 09:28:17 +0000

    first commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace vscode_netcore
 {
@@ -6,7 +6,7 @@ namespace vscode_netcore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Planet!");
         }
     }
 }
