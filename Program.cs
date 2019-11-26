0470af8c06fe6c89e15bccb6798f2eff8c369d09
Jamie Cansdale <jcansdale@gmail.com>
2019-11-26 09:08:22 +0000

    Hell World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vscode_netcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
